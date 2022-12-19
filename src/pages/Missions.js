2df1d33e2b8c3b0e4de27350090d4d4684b78fc5
storres20 <sanchotorres777@gmail.com>
2022-12-19 17:53:25 -0500

    Add table and render data for missions

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,5 +1,8 @@
+/* eslint-disable camelcase */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Table from 'react-bootstrap/Table'; /* React Bootstrap */
+import Button from 'react-bootstrap/Button'; /* React Bootstrap */
 import { getMissions } from '../redux/missions/missionSlice';
 
 function Missions() {
@@ -8,6 +11,7 @@ function Missions() {
 
   /* At the beginning cont => [], after dispatch cont => [{},{}...{}] */
   const cont = useSelector((state) => state.missions);
+  console.log(cont);
 
   /* For dispatching only once */
   useEffect(() => {
@@ -17,7 +21,27 @@ function Missions() {
   }, [dispatch, cont]);
 
   return (
-    <div>Missions</div>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+          <th> </th>
+        </tr>
+      </thead>
+      <tbody>
+        {cont.map((item) => (
+          <tr key={item.mission_id}>
+            <td><b>{item.mission_name}</b></td>
+            <td>{item.description}</td>
+            <td>{item.reserved ? 'Active Member' : 'NOT A MEMBER'}</td>
+            <td><Button variant={item.reserved ? 'outline-danger' : 'outline-secondary'}>{item.reserved ? 'Leave Mission' : 'Join Mission'}</Button></td>
+          </tr>
+        ))}
+
+      </tbody>
+    </Table>
   );
 }
 
