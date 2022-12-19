2fbed4c98a2adda065991073d4288b0169056d03
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-19 16:49:16 +0100

    Create navigation and add logo picture

--- src/App.js
+++ src/App.js
@@ -1,1 +1,33 @@
 
+import React from 'react';
+import './App.css';
+import {
+  Link, NavLink, Routes, Route,
+} from 'react-router-dom';
+
+const App = () => (
+  <>
+    <nav>
+      <Link className="logo" to="/"></Link>
+      <ul className="nav-links">
+        <li>
+          <NavLink className="nav-link" to="/">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink className="nav-link" to="/missions">Missions</NavLink>
+        </li>
+         <li>
+          <NavLink className="nav-link" to="/profile">My Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/myProfile" element={<Profile />} />
+    </Routes>
+  </>
+);
+
+export default App;
