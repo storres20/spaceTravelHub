14ec0a6172a4df1502f1c9f0763c0bfa7c64a5df
storres20 <sanchotorres777@gmail.com>
2022-12-19 17:55:07 -0500

    Fix code review's suggestions

--- src/pages/Missions.js
+++ src/pages/Missions.js
@@ -1,20 +1,14 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getMissions } from '../redux/missions/missionSlice';
 
 function Missions() {
   /* Setup Redux dispatch */
   const dispatch = useDispatch();
 
-  /* At the beginning cont => [], after dispatch cont => [{},{}...{}] */
-  const cont = useSelector((state) => state.missions);
-
-  /* For dispatching only once */
   useEffect(() => {
-    if (cont.length === 0) {
-      dispatch(getMissions());
-    }
-  }, [dispatch, cont]);
+    dispatch(getMissions());
+  }, [dispatch]);
 
   return (
     <div>Missions</div>
