2e4ab72b6498b35e12f94b7c3d182141c672347b
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-20 15:00:03 +0100

    feat: improve cards style margins

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,31 +1,35 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import '../App.css';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import Button from 'react-bootstrap/Button';
-
 
 function Rockets() {
   const { rockets } = useSelector((state) => state.rockets);
 
   return (
-    <CardGroup className="d-grid" style={{ padding: '10px', borderStyle: 'none' }}>
+    <CardGroup className="d-grid" style={{ marginTop: '2%', padding: '10px', borderStyle: 'none' }}>
       {rockets.map(({
         id,
         rocketName,
         description,
         flickrImages,
       }) => (
-        <Card key={id} className="d-flex flex-sm-column flex-md-row" style={{ marginBottom: '2%' }}>
-          <Card.Img src={flickrImages} />
-          <Card.Body>
-            <Card.Title>{rocketName}</Card.Title>
-            <Card.Text>
-              {description}
-            </Card.Text>
-            <Button variant="primary">Reserve Rocket</Button>
-          </Card.Body>
+        <Card key={id} className="d-flex flex-sm-column flex-md-row" style={{ marginBottom: '2%', borderStyle: 'none' }}>
+          <Col xs={6} md={3}>
+            <Card.Img src={flickrImages} />
+          </Col>
+          <Col xs={12} md={9}>
+            <Card.Body>
+              <Card.Title>{rocketName}</Card.Title>
+              <Card.Text>
+                {description}
+              </Card.Text>
+              <Button variant="primary">Reserve Rocket</Button>
+            </Card.Body>
+          </Col>
         </Card>
       ))}
     </CardGroup>
