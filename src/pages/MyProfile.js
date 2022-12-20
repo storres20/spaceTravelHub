38b1e45b861fe269cc4bd4de25a031e84f2081ad
storres20 <sanchotorres777@gmail.com>
2022-12-20 17:47:50 -0500

    Add joined missions to myprofile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -8,10 +8,34 @@ import { useSelector } from 'react-redux';
 const MyProfile = () => {
   const { rockets } = useSelector((state) => state.rockets);
 
+  /* Load REDUX missions' state */
+  const missions = useSelector((state) => state.missions);
+  /* filter() joining missions (reserved === true) */
+  const joinMissions = missions.filter((item) => item.reserved === true);
+
   return (
     <Container fluid>
+      {/* Missions joined */}
       <Row className="justify-content-around flex-nowrap">
-        {/* Put the code for the missions here */ }
+        <Col md={6} style={{ width: '40%' }}>
+          <Card border="light">
+            <Card.Body>
+              <Card.Title>My Missions</Card.Title>
+              <Card.Text>
+                <ul className="list-group">
+                  {
+                    joinMissions.map((join) => (
+                      <li key={join.mission_id} className="list-group-item">
+                        {join.mission_name}
+                      </li>
+                    ))
+                  }
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        {/* Rockets reserved */ }
         <Col md={6} style={{ width: '40%' }}>
           <Card border="light">
             <Card.Body>
