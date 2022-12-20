da3b2ce1d392d4351c11b3d0a11261a09e2491a6
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-20 10:46:30 +0100

    feat: add logger to te store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rockets from './rockets/rocketSlice';
 import missions from './missions/missionSlice';
+import logger from 'redux-logger'
 
-const store = configureStore({
-  reducer: {
-    rockets,
-    missions,
-  },
-});
+const reducer = {
+  rockets,
+  missions,
+}
+
+const store = configureStore(
+  reducer,
+  applyMiddleware(logger)
+);
 
 export default store;
