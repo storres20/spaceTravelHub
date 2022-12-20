78f3463a174c9390a6cf0775353c1ae65995202b
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-20 22:44:40 +0100

    feat: add rockets names to the rockets list

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
 import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const { rockets } = useSelector((state) => state.rockets);
 
   return (
-    <div>MyProfile</div>
+    <Container className="d-flex flex-column align-items-center">
+      <h2>My Missions</h2>
+      <ul className="list-group">
+        
+      </ul>
+      <h2>My Rockets</h2>
+      <ul className="list-group">
+        {rockets.map((rocket) => (
+          <li key={rocket.id} className="list-group-item">
+            {rocket.rocketName}
+          </li>
+        ))}
+      </ul>
+    </Container>
   );
 };
 
