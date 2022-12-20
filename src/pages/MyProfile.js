a4b344ce21a0ba9b196ca2ba136c7e0208080842
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2022-12-20 23:06:23 +0100

    feat: improve style of cards inside my profile page

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const { rockets } = useSelector((state) => state.rockets);
 
   return (
-    <Container className="d-flex flex-column align-items-center">
-      <h2>My Missions</h2>
-      <ul className="list-group">
-        
-      </ul>
-      <h2>My Rockets</h2>
-      <ul className="list-group">
-        {rockets.map((rocket) => (
-          <li key={rocket.id} className="list-group-item">
-            {rocket.rocketName}
-          </li>
-        ))}
-      </ul>
+    <Container fluid>
+      <Row className="justify-content-around flex-nowrap">
+        {/* Put the code for the missions here */ }
+        <Col md={6} style={{ width: '40%' }}>
+          <Card border="light">
+            <Card.Body>
+              <Card.Title>My Rockets</Card.Title>
+              <Card.Text>
+                <ul className="list-group">
+                  {rockets.map((rocket) => (
+                    <li key={rocket.id} className="list-group-item">
+                      {rocket.rocketName}
+                    </li>
+                  ))}
+                </ul>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
